580b9caeb1a7ec2b1be58a5dd1b57afeec4b5726
aminelebrini <aminelebrini4@gmail.com>
2025-12-01 14:44:11 +0100

    j'ai fait des modifications sur le header

--- templates/header.php
+++ templates/header.php
@@ -2,9 +2,9 @@
     <nav class="">
       <h1 class="">NovaCraft</h1>
       <ul class="">
-        <li><a href="#" class="">Home</a></li>
-        <li><a href="#" class="">Services</a></li>
-        <li><a href="#" class="">About Us</a></li>
+        <li><a href="../templates/layout.php" class="">Home</a></li>
+        <li><a href="../views/service.php" class="">Services</a></li>
+        <li><a href="../views/aboutus.php" class="">About Us</a></li>
         <li><a href="#" class="">Contact</a></li>
       </ul>
     </nav>
