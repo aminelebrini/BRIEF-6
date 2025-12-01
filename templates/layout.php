f21616f36d58236d7e1249085145853ec77f8f97
aminelebrini <aminelebrini4@gmail.com>
2025-12-01 17:14:10 +0100

    j'ai ajouté un lien des icons pour ajouté les icons

--- templates/layout.php
+++ templates/layout.php
@@ -1,4 +1,6 @@
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+
 <?php
 include __DIR__ . "/header.php";
 include __DIR__ . "/slide.php";
