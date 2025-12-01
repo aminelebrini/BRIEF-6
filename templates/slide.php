9cf5b1207f5b28ea4da1b1c0bb02f1693606f7be
aminelebrini <aminelebrini4@gmail.com>
2025-12-01 17:15:07 +0100

    j'ai ajouté quelque modifications sur le code ccs pour le rsponsive

--- templates/slide.php
+++ templates/slide.php
@@ -1,5 +1,7 @@
-<div class="bg-[url('https://img.freepik.com/free-vector/gradient-ui-ux-background_23-2149051556.jpg?semt=ais_hybrid&w=740&q=80')] h-full w-full bg-no-repeat bg-cover">
-    <div class="bg-black-500"></div>
-    <h1 class="text-white text-[40px]">NovaCraft</h1>
-    <button type="button" class="text-black bg-white">Explore Now</button>
+<div class="bg-[url('https://img.freepik.com/free-vector/gradient-ui-ux-background_23-2149051556.jpg?semt=ais_hybrid&w=740&q=80')] h-full bg-no-repeat bg-cover relative">
+    <div class="bg-black-500 w-full h-full absolute"></div>
+    <div class="flex flex-col items-center justify-center h-full">
+        <h1 class="text-white text-[120px] text-shadow-sm font-bold">NovaCraft</h1>
+        <button type="button" class="text-black bg-white p-[20px] rounded-[10px] w-[300px] text-[40px]">Explore Now</button>
+    </div>
 </div>
