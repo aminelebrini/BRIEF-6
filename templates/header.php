4e4b734529d94a64bdd8b8da6133abc878ef2173
aminelebrini <aminelebrini4@gmail.com>
2025-12-01 17:12:58 +0100

    j'ai ajouté quelque modifications sur le header en prend en concideration le responsive design

--- templates/header.php
+++ templates/header.php
@@ -1,11 +1,12 @@
-<header class="">
-    <nav class="">
-      <h1 class="">NovaCraft</h1>
-      <ul class="">
-        <li><a href="../templates/layout.php" class="">Home</a></li>
-        <li><a href="../views/service.php" class="">Services</a></li>
-        <li><a href="../views/aboutus.php" class="">About Us</a></li>
-        <li><a href="#" class="">Contact</a></li>
-      </ul>
+<header class="bg-white shadow-md flex flex-row p-[20px]">
+    <nav class="flex flex-row justify-between w-[98%]">
+      <h1 class="text-black text-[20px] font-bold">NovaCraft</h1>
+      <button type="button"><i class="fas fa-bars text-2xl"></i></button>
+      <!-- <ul class="flex flex-row justify-between gap-[20px] hidden">
+        <li><a href="../templates/layout.php" class="underline-non">Home</a></li>
+        <li><a href="../views/service.php" class="underline-non">Services</a></li>
+        <li><a href="../views/aboutus.php" class="underline-non">About Us</a></li>
+        <li><a href="../views/contact.php" class="underline-non">Contact</a></li>
+      </ul> -->
     </nav>
 </header>
