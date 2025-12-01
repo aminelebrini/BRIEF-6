3138c4fbcb614bf191305a0ea041224e0e019077
aminelebrini <aminelebrini4@gmail.com>
2025-12-01 14:47:04 +0100

    j'ai fait des modifications sur le slide

--- templates/slide.php
+++ templates/slide.php
@@ -1,4 +1,5 @@
-<div class="bg-[url('https://img.freepik.com/free-vector/gradient-ui-ux-elements-background_23-2149056159.jpg')] h-full w-full bg-no-repeat bg-cover">
+<div class="bg-[url('https://img.freepik.com/free-vector/gradient-ui-ux-background_23-2149051556.jpg?semt=ais_hybrid&w=740&q=80')] h-full w-full bg-no-repeat bg-cover">
+    <div class="bg-black-500"></div>
     <h1 class="text-white text-[40px]">NovaCraft</h1>
     <button type="button" class="text-black bg-white">Explore Now</button>
 </div>
