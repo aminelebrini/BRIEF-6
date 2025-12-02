13a2409d62cf6f20b061f2edcab1807f51d442ce
aminelebrini <aminelebrini4@gmail.com>
2025-12-02 11:35:23 +0100

    changes

--- templates/slide.php
+++ templates/slide.php
@@ -1,7 +1,10 @@
-<div class="bg-[url('https://img.freepik.com/free-vector/gradient-ui-ux-background_23-2149051556.jpg?semt=ais_hybrid&w=740&q=80')] h-full bg-no-repeat bg-cover relative">
-    <div class="bg-black-500 w-full h-full absolute"></div>
-    <div class="flex flex-col items-center justify-center h-full">
-        <h1 class="text-white text-[120px] text-shadow-sm font-bold">NovaCraft</h1>
-        <button type="button" class="text-black bg-white p-[20px] rounded-[10px] w-[300px] text-[40px]">Explore Now</button>
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+
+<div class="relative h-screen bg-cover bg-center bg-no-repeat md:h-[90vh] bg-[url('https://img.freepik.com/free-vector/gradient-ui-ux-background_23-2149051556.jpg')]">
+    <div class="absolute inset-0 bg-black/60"></div>
+    <div class="relative flex flex-col items-center justify-center h-full">
+        <h1 class="text-white text-[70px] font-bold drop-shadow-lg">NovaCraft</h1>
+        <button type="button" class="mt-4 text-black bg-white px-10 py-5 rounded-xl text-2xl font-medium shadow-xl md:text-xl">Explore Now</button>
     </div>
 </div>
