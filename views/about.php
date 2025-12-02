8790394e88ed57d62fc4e5062e8ff27ec481537f
aminelebrini <aminelebrini4@gmail.com>
2025-12-02 11:56:28 +0100

    j'ai changé la structure de code html de cette partie

--- views/about.php
+++ views/about.php
@@ -1,6 +1,9 @@
-<section class="flex flex-col justify-center items-center">
-    <h1>About Us</h1>
-    <p>At NovaCraft Studio, we bring ideas to life through innovative design. As a full-service graphic and UI/UX design agency, we specialize in creating visually stunning and user-friendly experiences that connect brands with their audiences.<br>
-    Our team of creative professionals combines artistic vision with strategic thinking to deliver designs that not only look amazing but also drive engagement and results. From building a cohesive brand identity to crafting intuitive digital interfaces, we are dedicated to helping businesses stand out in today’s competitive market.<br>
-    At NovaCraft Studio, design is more than just aesthetics—it’s about creating meaningful experiences that leave a lasting impression.</p>
-</section>
+<div class="flex flex-col md:flex-row md:items-center md:justify-around md:w-[90%] md:h-[81vh] gap-5 py-10 mx-auto">
+        <div class="w-[100%] flex flex-col justify-center items-center">
+            <h1 class="text-[40px] font-bold">About Us</h1>
+            <p class="w-[95%] text-[25px]">At NovaCraft Studio, we bring ideas to life through innovative design. As a full-service graphic and UI/UX design agency, we specialize in creating visually stunning and user-friendly experiences that connect brands with their audiences.<br>
+            Our team of creative professionals combines artistic vision with strategic thinking to deliver designs that not only look amazing but also drive engagement and results. From building a cohesive brand identity to crafting intuitive digital interfaces, we are dedicated to helping businesses stand out in today’s competitive market.<br>
+            At NovaCraft Studio, design is more than just aesthetics—it’s about creating meaningful experiences that leave a lasting impression.</p>
+        </div>
+        <img src="../images/—Pngtree—vector about us images_16023330.png" class="md:w-[450px] md:h-[450px]" alt="bg">
+</div>
