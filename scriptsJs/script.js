a317b12dccbdfb9bf08f5785f232dbf29e453b75
aminelebrini <aminelebrini4@gmail.com>
2025-12-03 15:04:50 +0100

    last change

--- scriptsJs/script.js
+++ scriptsJs/script.js
@@ -7,9 +7,3 @@ document.getElementById('close').addEventListener('click', ()=>{
      Side.classList.add("translate-x-full");
 });
 
-
-document.addEventListener('submit', ()=>{
-    const Fname = document.getElementById('fullname').value;
-    const Email = document.getElementById('email').value;
-    const Message = document.getElementById('message').value; 
-});
