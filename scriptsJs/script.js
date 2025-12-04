312388fbdcb6d3afb2468a8af75b5435bd73c9de
aminelebrini <aminelebrini4@gmail.com>
2025-12-04 11:19:49 +0100

    changes1

--- scriptsJs/script.js
+++ scriptsJs/script.js
@@ -7,7 +7,7 @@ document.getElementById('close').addEventListener('click', ()=>{
      Side.classList.add("translate-x-full");
 });
 
-
 document.getElementById('btnslide').addEventListener('click', ()=>{
     window.location.href ="index.php?page=service";
 })
+
