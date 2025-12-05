9b7c49f7e616352a61802123af13c0d7fd08e7e7
aminelebrini <aminelebrini4@gmail.com>
2025-12-05 09:12:28 +0100

    changes

--- templates/side-bar.php
+++ templates/side-bar.php
@@ -1,9 +1,13 @@
-<div class="side-bar fixed top-0 right-0 h-full w-[60%] bg-white text-white 
+<div class="side-bar flex flex-col fixed top-0 right-0 h-full w-[60%] bg-white  
             translate-x-full transition-transform duration-300 p-5 z-50">
-    <ul class="flex flex-col gap-4 text-lg mt-10 text-black">
-        <div class="close-side-bar">
-            <button type="button" class="md:hidden" id="close"><i class="fas fa-multiply text-2xl"></i></button>
-        </div>
+
+    <div class="closeB flex flex-row items-center justify-start text-black">
+        <button type="button" class="md:hidden mb-5" id="close">
+            <i class="fas fa-multiply text-2xl"></i>
+        </button>
+    </div>
+
+    <ul class="nav flex flex-col gap-4 text-lg mt-5 text-black">
         <li><a href="/home" class="underline-non">Home</a></li>
         <li><a href="/service" class="underline-non">Services</a></li>
         <li><a href="/about" class="underline-non">About Us</a></li>
